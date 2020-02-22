294770d2dbf1b20f5cc0b0d6334b6b5c8ab0a2f2
Randy Olshen <rolshen@aol.com>
2020-02-22 12:03:57 -0800

    update

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 // const exphbs = require("express-handlebars");
-var exphbs = require("express-handlebars");
+
 
 const app = express();
 
@@ -14,6 +14,9 @@ var port = process.env.PORT || 8060;
 
 // const routes = require("./controllers/burgersController.js");
 // app.use("/", routes);
+
+var exphbs = require("express-handlebars");
+
 app.get("/", function(req, res){
   console.log("hello simple");
   res.send("hello simple")
