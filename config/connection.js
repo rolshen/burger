bbf8f088860a7904b05be4d67bcf0f3d350f9bdd
Randy Olshen <rolshen@aol.com>
2020-02-22 10:34:16 -0800

    update

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,25 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  port: 3306,
-  host: "localhost",
-  user: "root",
-  password: "RaOl91559590!",
-  database: "burgers_db"
-});
+// var connection = mysql.createConnection({
+//   port: 3306,
+//   host: "localhost",
+//   user: "root",
+//   password: "RaOl91559590!",
+//   database: "burgers_db"
+// });
+
+var connection;
+
+  if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+  } else {
+    connection = mysql.createConnection({
+      host: "muowdopceqgxjn2b.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+      user: "wppmqd6cxq39er4c",
+      password: "z7hwuno97b7kub6r",
+      database: "j4jti3obcm1stb2r"
+    });
+  };
 
 connection.connect(function(err) {
   if (err) {
