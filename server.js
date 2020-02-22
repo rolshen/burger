cd807b9a1008bb6dcdebc25291d8827c22ea1312
Randy Olshen <rolshen@aol.com>
2020-02-22 09:54:59 -0800

    update

--- server.js
+++ server.js
@@ -11,9 +11,16 @@ app.use(express.static("public"));
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-const routes = require("./controllers/burgersController.js");
-app.use("/", routes);
+// const routes = require("./controllers/burgersController.js");
+// app.use("/", routes);
 
+router.get("/", function(req, res) {
+  console.log("Route Path");
+
+  console.log("Diplayed Burgers");
+  res.send("display burger");
+
+});
 
 app.listen(port, function() {
   console.log("App listening on PORT " + port);
