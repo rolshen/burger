4feaed210de28e061f2eb0c6cbcb90f386c98be4
Randy Olshen <rolshen@aol.com>
2020-02-22 11:45:33 -0800

    update

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 const express = require("express");
-const bodyParser = require("body-parser");
-const exphbs = require("express-handlebars");
+// const bodyParser = require("body-parser");
+// const exphbs = require("express-handlebars");
 
 const app = express();
 
 var port = process.env.PORT || 8060;
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static("public"));
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(express.static("public"));
 
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
+// app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+// app.set("view engine", "handlebars");
 
 // const routes = require("./controllers/burgersController.js");
 // app.use("/", routes);
