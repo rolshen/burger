c9a1842c901aac9a522b320039c4d03410209ed2
Randy Olshen <rolshen@aol.com>
2020-02-22 12:02:28 -0800

    update

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const exphbs = require("express-handlebars");
+// const exphbs = require("express-handlebars");
+var exphbs = require("express-handlebars");
 
 const app = express();
 
