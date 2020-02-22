5040861974e8052074f1eeb081aa2e2b00a400ef
Randy Olshen <rolshen@aol.com>
2020-02-22 12:28:28 -0800

    update

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-// const exphbs = require("express-handlebars");
-
+const exphbs = require("express-handlebars");
 
 const app = express();
 
 var port = process.env.PORT || 8060;
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static("public"));
 
-app.get("/", function(req, res){
-    console.log("hello simple");
-    res.send("hello simple")
-    
-  }) 
-  
-  
-  app.listen(port, function() {
-    console.log("App listening on PORT " + port);
-  });
-  
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+const routes = require("./controllers/burgersController.js");
+app.use("/", routes);
+
+
+app.listen(port, function() {
+  console.log("App listening on PORT " + port);
+});
